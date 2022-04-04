f0aa7530aec06e8dd7138b50de98e03ca4e565ed
hhw <1918697599@qq.com>
2022-04-04 20:41:10 +0800

    新添加了登录按钮

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let btn = document.querySelector('button');
+let an = document.querySelector('.btn');
+
+//
 console.log(btn);
 btn.addEventListener('click', function() {
     console.log(11);
     alert('11');
 });
+
+an.addEventListener('click', function() {
+    alert(11);
+})
